27dfb595fa6757a8acc26759bc198842d7b77c57
ajlopez <webmaster@ajlopez.com>
2013-09-14 19:38:22 -0300

    Parse binary expression

--- test/parser.js
+++ test/parser.js
@@ -1,20 +1,56 @@
 
 var parser = require('../lib/parser');
     
-exports['parser integer'] = function (test) {
+exports['parse integer'] = function (test) {
     var result = parser.parseExpression('1');
     test.ok(result);
     test.equal(result.compile(), '1');
 }
     
-exports['compile variable'] = function (test) {
+exports['parse variable'] = function (test) {
     var result = parser.parseExpression('$k');
     test.ok(result);
     test.equal(result.compile(), '$k');
 }
     
-exports['compile single quoted string'] = function (test) {
+exports['parse single quoted string'] = function (test) {
     var result = parser.parseExpression("'foo'");
     test.ok(result);
     test.equal(result.compile(), "'foo'");
+}
+
+exports['parse integer sum'] = function (test) {
+    var result = parser.parseExpression("1+2");
+    test.ok(result);
+    test.equal(result.compile(), "1 + 2");
+}
+
+exports['parse variable subtract'] = function (test) {
+    var result = parser.parseExpression("$a-$b");
+    test.ok(result);
+    test.equal(result.compile(), "$a - $b");
+}
+
+exports['parse integer subtract'] = function (test) {
+    var result = parser.parseExpression("1 - 2");
+    test.ok(result);
+    test.equal(result.compile(), "1 - 2");
+}
+
+exports['parse variable sum'] = function (test) {
+    var result = parser.parseExpression("$a+$b");
+    test.ok(result);
+    test.equal(result.compile(), "$a + $b");
+}
+
+exports['parse variable multiply'] = function (test) {
+    var result = parser.parseExpression("$a*$b");
+    test.ok(result);
+    test.equal(result.compile(), "$a * $b");
+}
+
+exports['parse variable divide'] = function (test) {
+    var result = parser.parseExpression("$a/$b");
+    test.ok(result);
+    test.equal(result.compile(), "$a / $b");
 }
