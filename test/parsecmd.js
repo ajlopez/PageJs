96e0e1c22bb137cfd9fc5cbc8a823785e0f1ef92
ajlopez <webmaster@ajlopez.com>
2013-09-15 16:07:44 -0300

    Parse first command

---
+++ test/parsecmd.js
@@ -0,0 +1,8 @@
+
+var parser = require('../lib/parser');
+    
+exports['parse assignment'] = function (test) {
+    var result = parser.parseCommand('$a=1');
+    test.ok(result);
+    test.equal(result.compile(), '$a = 1;');
+}
